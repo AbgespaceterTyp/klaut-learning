97b306374690052dbf34c5d88e6bc66c56bda461
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-05-02 10:41:38 +0200

    IModelService: added methods for model creation, deletion and training

--- backend/src/main/java/de/htwg/klaut/backend/service/IModelService.java
+++ backend/src/main/java/de/htwg/klaut/backend/service/IModelService.java
@@ -5,6 +5,14 @@ import de.htwg.klaut.backend.model.db.Model;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Set;
+
 public interface IModelService {
     Page<Model> findAll(Pageable pageable);
+
+    Model createModel(String modelName, String modelDescription, ModelParams modelParams, Set<String> sourceUrls);
+
+    void trainsModel(String modelId, ModelParams modelParams, Set<String> sourceUrls);
+
+    void deleteModel(String modelId);
 }
--- backend/src/main/java/de/htwg/klaut/backend/service/Word2VecModelService.java
+++ backend/src/main/java/de/htwg/klaut/backend/service/Word2VecModelService.java
@@ -6,16 +6,34 @@ import de.htwg.klaut.backend.repository.ModelRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public class Word2VecModelService implements IModelService{
+import java.util.Set;
+
+public class Word2VecModelService implements IModelService {
 
     private final ModelRepository modelRepository;
 
-    public Word2VecModelService(ModelRepository modelRepository){
+    public Word2VecModelService(ModelRepository modelRepository) {
         this.modelRepository = modelRepository;
     }
 
     @Override
     public Page<Model> findAll(Pageable pageable) {
         return modelRepository.findAll(pageable);
+    }
+
+    @Override
+    public Model createModel(String modelName, String modelDescription, ModelParams modelParams, Set<String> sourceUrls) {
+        // TODO LG implement
+        return null;
+    }
+
+    @Override
+    public void trainsModel(String modelId, ModelParams modelParams, Set<String> sourceUrls) {
+        // TODO LG implement
+    }
+
+    @Override
+    public void deleteModel(String modelId) {
+        // TODO LG implement
     }
 }
