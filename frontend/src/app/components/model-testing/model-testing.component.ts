b130bf5a81d9e7e349fd306336c3faaca09329bd
Max Stenke <max_stenke@yahoo.de>
2018-05-31 14:56:11 +0200

    Add placeholder

--- frontend/src/app/components/model-testing/model-testing.component.ts
+++ frontend/src/app/components/model-testing/model-testing.component.ts
@@ -4,9 +4,10 @@ import {TrainingData} from '../../_models/training';
 @Component({selector: 'app-model-testing', templateUrl: './model-testing.component.html', styleUrls: ['./model-testing.component.scss']})
 export class ModelTestingComponent implements OnInit {
   @Input('trainingData')trainingData : [TrainingData];
+  @Input('modelId')modelId : String;
 
 
-  selectedTrainingData: String;
+  selectedTrainingData: String = null;
 
   tdata: [{
     endTimeLong: number;
@@ -19,9 +20,10 @@ export class ModelTestingComponent implements OnInit {
   ngOnInit() {
     this.tdata.pop();
     this.trainingData.forEach(element => {
+      let d: Date = new Date(element.lastTrainingEnd);
       this.tdata.push({
         endTimeLong: element.lastTrainingEnd,
-        endTime: new Date(element.lastTrainingEnd).toDateString(),
+        endTime: d.toDateString() + ' ' + d.toLocaleTimeString(),
         url: element.modelUrl
       })
     });
