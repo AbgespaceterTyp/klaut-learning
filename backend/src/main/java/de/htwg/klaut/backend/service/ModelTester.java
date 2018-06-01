d47890fbf0a21f44dd05180410ab0d967c54164b
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-06-01 13:33:45 +0200

    ModelTester: handle NullPointerExceptions when searching for synonyms with dl4j

--- backend/src/main/java/de/htwg/klaut/backend/service/ModelTester.java
+++ backend/src/main/java/de/htwg/klaut/backend/service/ModelTester.java
@@ -1,8 +1,6 @@
 package de.htwg.klaut.backend.service;
 
 import de.htwg.klaut.backend.exception.SourceNotFoundException;
-import de.htwg.klaut.backend.model.db.ModelTrainingData;
-import de.htwg.klaut.backend.model.dto.ModelTrainingDataDto;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -15,6 +13,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -36,12 +35,19 @@ public class ModelTester {
         final File modelFile = new File(System.getProperty("java.io.tmpdir") + modelFileName + ".model");
         modelFile.deleteOnExit();
 
-        try (FileOutputStream fos = new FileOutputStream(modelFile)){
+        try (FileOutputStream fos = new FileOutputStream(modelFile)) {
             IOUtils.copy(sourceFile.get(), fos);
             final Word2Vec word2VecModelToTest = WordVectorSerializer.readWord2VecModel(modelFile);
             // Important: test word must be in lower case or dl4j will throw NullPointerException
             // (because all words in vocabulary are stored in lower case)
-            return word2VecModelToTest.wordsNearestSum(testWord.toLowerCase(), 10);
+            try {
+                return word2VecModelToTest.wordsNearestSum(testWord.toLowerCase(), 10);
+            } catch (NullPointerException e) {
+                // Dl4J throws NullPointerException when the given test word is not contained in vocabulary
+                // so NullPointerException will be ignored here and a empty collection returned
+                log.debug("Failed to find value for test word {}", testWord, e);
+                return Collections.emptyList();
+            }
         } catch (IOException e) {
             log.error("Failed to test model", e);
             throw new SourceNotFoundException(sourceUrl);
