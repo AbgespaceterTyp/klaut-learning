564bcfd7f77f60df4d2d097b64692bacbbfb7b30
Lukas Gross <lu851gro@htwg-konstanz.de>
2018-06-15 09:27:49 +0200

    fixed copy paste magic

--- backend/src/main/java/de/htwg/klaut/backend/exception/OrganizationCreationException.java
+++ backend/src/main/java/de/htwg/klaut/backend/exception/OrganizationCreationException.java
@@ -3,10 +3,10 @@ package de.htwg.klaut.backend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "failed to create model")
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "failed to create organization")
 public class OrganizationCreationException extends RuntimeException {
 
-    public OrganizationCreationException(String modelName) {
-        super("failed to create model with name " + modelName);
+    public OrganizationCreationException(String name) {
+        super("failed to create organization with name " + name);
     }
 }
